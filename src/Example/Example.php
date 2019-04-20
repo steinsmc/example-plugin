ee5dbc30321b0ce28f159a93322b14349f6f074b
William Wang <dhdjwjy@gmail.com>
2019-04-20 15:11:31 -0400

    Example

---
+++ src/Example/Example.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace Example;
+
+
+use Amadeus\IO\Logger;
+use Amadeus\Process;
+use Amadeus\Plugin\Basic\PluginBase;
+use Amadeus\Plugin\Listener\Listener;
+
+class Example extends PluginBase implements Listener
+{
+    public function __construct(){
+        //Process::getPluginManager()->register($this,'Example');
+    }
+    public function onLoading(){
+        Logger::printLine('I AM LOADING!!!!!',Logger::LOG_INFORM);
+    }
+    public function onLoaded(){
+        Logger::printLine('ALL PLUGINS ARE LOADED!!!!1',Logger::LOG_INFORM);
+    }
+    public function getName(){
+        return 'Example Plugin';
+    }
+}
